ea9c40478a8485c55ab7c4069bb5c272f4277337
rhum <lucas.rioust@gmail.com>
2021-11-07 12:39:31 +0100

    depthwrite fix 2

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -627,7 +627,7 @@ const setupScene = (gltf, d) => {
     d.scene.add( light2 )
 
     traverseMaterials(d.scene, (material) => {
-        material.depthWrite = !node.material.transparent
+        material.depthWrite = !material.transparent
         if (material.map) material.map.encoding = d.THREE.sRGBEncoding
         if (material.emissiveMap) material.emissiveMap.encoding = d.THREE.sRGBEncoding
         if (material.map || material.emissiveMap) material.needsUpdate = true;
