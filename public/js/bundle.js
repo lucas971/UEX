edcecd710fd01d70edebf71c9645a2c7fa0ef3a8
rhum <lucas.rioust@gmail.com>
2021-11-09 17:09:20 +0100

    test outline

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -940,6 +940,14 @@ const InitializeSound =() => {
 //#endregion
 
 //#region SHADERS
+
+import { EffectComposer } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/postprocessing/EffectComposer.js";
+import { RenderPass } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/postprocessing/RenderPass.js";
+import { ShaderPass } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/postprocessing/ShaderPass.js";
+import { FXAAShader } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/shaders/FXAAShader.js";
+import { Pass } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/postprocessing/Pass.js";
+
+//#region OCEAN
 let ocean_mat
 const ocean_uniforms = {
     iTime: {
@@ -1164,6 +1172,224 @@ const ocean_frag = "// \"Wind Waker Ocean\" by @Polyflare (29/1/15)\n" +
     "    \t}\n" +
     "    }\n" +
     "}\n"
+//#endregion
+
+//#region OUTLINE
+
+// Follows the structure of
+// 		https://github.com/mrdoob/three.js/blob/master/examples/jsm/postprocessing/OutlinePass.js
+class CustomOutlinePass extends Pass {
+    constructor(resolution, scene, camera) {
+        super();
+
+        this.renderScene = scene;
+        this.renderCamera = camera;
+        this.resolution = new THREE.Vector2(resolution.x, resolution.y);
+
+        this.fsQuad = new Pass.FullScreenQuad(null);
+        this.fsQuad.material = this.createOutlinePostProcessMaterial();
+
+        // Create a buffer to store the normals of the scene onto
+        const normalTarget = new THREE.WebGLRenderTarget(
+            this.resolution.x,
+            this.resolution.y
+        );
+        normalTarget.texture.format = THREE.RGBFormat;
+        normalTarget.texture.minFilter = THREE.NearestFilter;
+        normalTarget.texture.magFilter = THREE.NearestFilter;
+        normalTarget.texture.generateMipmaps = false;
+        normalTarget.stencilBuffer = false;
+        this.normalTarget = normalTarget;
+
+        this.normalOverrideMaterial = new THREE.MeshNormalMaterial();
+    }
+
+    dispose() {
+        this.normalTarget.dispose();
+        this.fsQuad.dispose();
+    }
+
+    setSize(width, height) {
+        this.normalTarget.setSize(width, height);
+        this.resolution.set(width, height);
+
+        this.fsQuad.material.uniforms.screenSize.value.set(
+            this.resolution.x,
+            this.resolution.y,
+            1 / this.resolution.x,
+            1 / this.resolution.y
+        );
+    }
+
+    render(renderer, writeBuffer, readBuffer) {
+        // Turn off writing to the depth buffer
+        // because we need to read from it in the subsequent passes.
+        const depthBufferValue = writeBuffer.depthBuffer;
+        writeBuffer.depthBuffer = false;
+
+        // 1. Re-render the scene to capture all normals in texture.
+        // Ideally we could capture this in the first render pass along with
+        // the depth texture.
+        renderer.setRenderTarget(this.normalTarget);
+
+        const overrideMaterialValue = this.renderScene.overrideMaterial;
+        this.renderScene.overrideMaterial = this.normalOverrideMaterial;
+        renderer.render(this.renderScene, this.renderCamera);
+        this.renderScene.overrideMaterial = overrideMaterialValue;
+
+        this.fsQuad.material.uniforms["depthBuffer"].value =
+            readBuffer.depthTexture;
+        this.fsQuad.material.uniforms[
+            "normalBuffer"
+            ].value = this.normalTarget.texture;
+        this.fsQuad.material.uniforms["sceneColorBuffer"].value =
+            readBuffer.texture;
+
+        // 2. Draw the outlines using the depth texture and normal texture
+        // and combine it with the scene color
+        if (this.renderToScreen) {
+            // If this is the last effect, then renderToScreen is true.
+            // So we should render to the screen by setting target null
+            // Otherwise, just render into the writeBuffer that the next effect will use as its read buffer.
+            renderer.setRenderTarget(null);
+            this.fsQuad.render(renderer);
+        } else {
+            renderer.setRenderTarget(writeBuffer);
+            this.fsQuad.render(renderer);
+        }
+
+        // Reset the depthBuffer value so we continue writing to it in the next render.
+        writeBuffer.depthBuffer = depthBufferValue;
+    }
+
+    get vertexShader() {
+        return `
+			varying vec2 vUv;
+			void main() {
+			  vUv = uv;
+			  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+			}
+			`;
+    }
+    get fragmentShader() {
+        return `
+			#include <packing>
+			// The above include imports "perspectiveDepthToViewZ"
+			// and other GLSL functions from ThreeJS we need for reading depth.
+			uniform sampler2D sceneColorBuffer;
+			uniform sampler2D depthBuffer;
+			uniform sampler2D normalBuffer;
+			uniform float cameraNear;
+  		uniform float cameraFar;
+  		uniform vec4 screenSize;
+      uniform vec3 outlineColor;
+      uniform vec4 multiplierParameters;
+      uniform int debugVisualize;
+			varying vec2 vUv;
+			// Helper functions for reading from depth buffer.
+			float readDepth (sampler2D depthSampler, vec2 coord) {
+				float fragCoordZ = texture2D(depthSampler, coord).x;
+				float viewZ = perspectiveDepthToViewZ( fragCoordZ, cameraNear, cameraFar );
+				return viewZToOrthographicDepth( viewZ, cameraNear, cameraFar );
+			}
+			float getLinearDepth(vec3 pos) {
+				return -(viewMatrix * vec4(pos, 1.0)).z;
+			}
+			float getLinearScreenDepth(sampler2D map) {
+		    	vec2 uv = gl_FragCoord.xy * screenSize.zw;
+		    	return readDepth(map,uv);
+			}
+			// Helper functions for reading normals and depth of neighboring pixels.
+			float getPixelDepth(int x, int y) {
+				// screenSize.zw is pixel size 
+				// vUv is current position
+				return readDepth(depthBuffer, vUv + screenSize.zw * vec2(x, y));
+			}
+			vec3 getPixelNormal(int x, int y) {
+				return texture2D(normalBuffer, vUv + screenSize.zw * vec2(x, y)).rgb;
+			}
+      float saturate(float num) {
+        return clamp(num, 0.0, 1.0);
+      }
+			void main() {
+				vec4 sceneColor = texture2D(sceneColorBuffer, vUv);
+				float depth = getPixelDepth(0, 0);
+				vec3 normal = getPixelNormal(0, 0);
+				// Get the difference between depth of neighboring pixels and current.
+				float depthDiff = 0.0;
+		  	depthDiff += abs(depth - getPixelDepth(1, 0));
+		  	depthDiff += abs(depth - getPixelDepth(-1, 0));
+		  	depthDiff += abs(depth - getPixelDepth(0, 1));
+		  	depthDiff += abs(depth - getPixelDepth(0, -1));
+		  	// Get the difference between normals of neighboring pixels and current
+		  	float normalDiff = 0.0;
+		  	normalDiff += distance(normal, getPixelNormal(1, 0));
+		  	normalDiff += distance(normal, getPixelNormal(0, 1));
+		  	normalDiff += distance(normal, getPixelNormal(0, 1));
+		  	normalDiff += distance(normal, getPixelNormal(0, -1));
+        normalDiff += distance(normal, getPixelNormal(1, 1));
+        normalDiff += distance(normal, getPixelNormal(1, -1));
+        normalDiff += distance(normal, getPixelNormal(-1, 1));
+        normalDiff += distance(normal, getPixelNormal(-1, -1));
+        // Apply multiplier & bias to each 
+        float depthBias = multiplierParameters.x;
+        float depthMultiplier = multiplierParameters.y;
+        float normalBias = multiplierParameters.z;
+        float normalMultiplier = multiplierParameters.w;
+        depthDiff = depthDiff * depthMultiplier;
+        depthDiff = saturate(depthDiff);
+        depthDiff = pow(depthDiff, depthBias);
+        normalDiff = normalDiff * normalMultiplier;
+        normalDiff = saturate(normalDiff);
+        normalDiff = pow(normalDiff, normalBias);
+		  	float outline = normalDiff + depthDiff;
+			
+		  	// Combine outline with scene color.
+		  	vec4 outlineColor = vec4(outlineColor, 1.0);
+		  	gl_FragColor = vec4(mix(sceneColor, outlineColor, outline));
+        // For debug visualization of the different inputs to this shader.
+        if (debugVisualize == 1) {
+          gl_FragColor = sceneColor;
+        }
+        if (debugVisualize == 2) {
+          gl_FragColor = vec4(vec3(depth), 1.0);
+        }
+        if (debugVisualize == 3) {
+          gl_FragColor = vec4(normal, 1.0);
+        }
+			}
+			`;
+    }
+
+    createOutlinePostProcessMaterial() {
+        return new THREE.ShaderMaterial({
+            uniforms: {
+                debugVisualize: { value: 0 },
+                sceneColorBuffer: {},
+                depthBuffer: {},
+                normalBuffer: {},
+                outlineColor: { value: new THREE.Color(0xffffff) },
+                //4 scalar values packed in one uniform: depth multiplier, depth bias, and same for normals.
+                multiplierParameters: { value: new THREE.Vector4(1, 1, 1, 1) },
+                cameraNear: { value: this.renderCamera.near },
+                cameraFar: { value: this.renderCamera.far },
+                screenSize: {
+                    value: new THREE.Vector4(
+                        this.resolution.x,
+                        this.resolution.y,
+                        1 / this.resolution.x,
+                        1 / this.resolution.y
+                    ),
+                },
+            },
+            vertexShader: this.vertexShader,
+            fragmentShader: this.fragmentShader,
+        });
+    }
+}
+
+
+//#endregion
 
 const InitializeShaders = (d) => {
     ocean_mat = new THREE.ShaderMaterial({
@@ -1171,6 +1397,39 @@ const InitializeShaders = (d) => {
         vertexShader: ocean_vert,
         fragmentShader: ocean_frag
     })
+    // Set up post processing
+    // Create a render target that holds a depthTexture so we can use it in the outline pass
+    // See: https://threejs.org/docs/index.html#api/en/renderers/WebGLRenderTarget.depthBuffer
+    const depthTexture = new d.THREE.DepthTexture();
+    const renderTarget = new d.THREE.WebGLRenderTarget(
+        window.innerWidth,
+        window.innerHeight,
+        {
+            depthTexture: depthTexture,
+            depthBuffer: true,
+        }
+    );
+
+    // Initial render pass.
+    d.composer = new EffectComposer(d.renderer, renderTarget);
+    const pass = new RenderPass(d.scene, d.camera);
+    d.composer.addPass(pass);
+
+    // Outline pass.
+    const customOutline = new CustomOutlinePass(
+        new d.THREE.Vector2(window.innerWidth, window.innerHeight),
+        d.scene,
+        d.camera
+    );
+    d.composer.addPass(customOutline);
+    
+    // Antialias pass.
+    const effectFXAA = new ShaderPass(FXAAShader);
+    effectFXAA.uniforms["resolution"].value.set(
+        1 / window.innerWidth,
+        1 / window.innerHeight
+    );
+    d.composer.addPass(effectFXAA);
 }
 
 const UpdateUniforms = (delta) => {
@@ -1272,7 +1531,7 @@ const animate = () => {
     if (activeScene === cityActiveScene) {
         UpdateCity(threeData)
     }
-    render()
+    threeData.composer.render()
 }
 
 const render = () => {
