24f84aae002f0bb1b270a38e26f17360619ecfa4
rhum <lucas.rioust@gmail.com>
2021-11-13 18:17:37 +0100

    fix id check

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -539,7 +539,11 @@ const UpdateView = () => {
     const totalList = futurList.concat(incluList).concat(ecoList).concat(inoList)
     
     for (let i = 0; i < totalList.length; i++) {
-        if (currentProgress.indexOf(totalList[i].id) >= 0) {
+        let idToTest = ''
+        if (totalList[i].id && totalList[i].id.includes('collectible-')) {
+            idToTest = totalList[id].replace('collectible-', '')
+        }
+        if (currentProgress.indexOf(idToTest) >= 0) {
             totalList[i].src = link_v_icon
         } else {
             totalList[i].src = link_x_icon
