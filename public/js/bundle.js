e4b2d7b959c63dbad8081a413f2fe0a8563d727e
rhum <lucas.rioust@gmail.com>
2021-11-17 15:04:23 +0100

    tutorial

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -313,7 +313,7 @@ const InitializeIcons = (d) => {
             icons = data["icons"]
             GenerateHtml(d)
             InitializeIconsPosition(d)
-            //InitializeTutorial()
+            InitializeTutorial()
             document.getElementById("loading-screen-stopper").click()
         },
         (error) => {
